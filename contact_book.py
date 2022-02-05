fce6b9ab233b76a181d28fbcf66f3ff8e22927e5
Daphne554 <daphnekodua@gmail.com>
2022-02-05 15:10:18 +0000

    New options added

--- contact_book.py
+++ contact_book.py
@@ -1,6 +1,4 @@
-name = input("Enter your name: ")
-print(name + "'s Contact Book")
-
+print("THE CONTACT BOOK")
 def add():
     name = input("Name: ")
     number = input("Mobile: ")
@@ -10,23 +8,57 @@ def add():
     with open("contactbook.txt", 'a') as c:
         c.write("Name: " + name + " | Mobile: " + number + " | Phone: " + number2 + " | Email: " + email + "\n")
 
+
 def view():
     with open("contactbook.txt",'r') as c:
         for line in c.readlines():
             print(line.rstrip())
 
 
+def delete():
+    del_info = input("Enter contact name to delete all contact information: ").lower()
+    with open("contactbook.txt",'r') as c:
+        file = c.readlines()
+    with open("contactbook.txt",'r') as c:
+        for line in file:
+            words = line.strip("\n").lower().split(' ')
+            if del_info.lower() not in words:
+                print(line)
+
+
+def search():
+    search_name = input("Enter contact name to search: ").lower()
+    with open("contactbook.txt", 'r') as c:
+        file = c.readlines()
+    with open("contactbook.txt", 'r') as c:
+        for line in file:
+            words = line.strip("\n").lower()
+            if search_name.lower() in words:
+                print(line)
+            else:
+                print("Contact not found! ")
+
 while True:
-    option = input("Would you like to add, view or exit contact book (add/view/exit): ")
-    if option == "exit":
+    option = int(input(" 1.Add new contact\n 2.View contact list\n 3.Search contact\n 4.Delete "
+                       "contact\n 5.Exit contact book\n 0.To begin to return to options\n Enter option number: "))
+    if option == 5:
+        quit()
+
+    if option == 0:
         continue
 
-    if option == "add":
+    if option == 1:
         add()
 
-    elif option == "view":
+    elif option == 2:
         view()
 
+    elif option == 3:
+        search()
+
+    elif option == 4:
+        delete()
+
     else:
-        print("Invalid option! Enter add/view/exit: ")
+        print("Invalid number ! Type number assigned option! ")
         break
